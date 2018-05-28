b3310683a49c3783b7a949d831c7ea74e1f168e6
Fed-up <tomcurphey12@gmail.com>
2018-05-28 18:13:11 +1000

    up to edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,135 @@
 import React, { Component } from "react";
 import "./App.css";
+import Styled from "styled-components";
+
+const FormWrapper = Styled.div`
+  margin-left: 40px;
+  margin-top: 40px;
+`;
+
+const RecipeUl = Styled.ul`
+  margin-top: 100px;
+`;
+
+const ListLi = Styled.li`
+    display: grid;
+    grid-gap: 10px;
+    grid-template-columns: [col] 200px [col] 100px [col] 100px [col] 100px ;
+    grid-template-rows: [row] 40px ;
+    list-style-type: none;
+    text-align: left;
+`;
+
+const ListItem = Styled.div`
+  background-color: rgba(255, 255, 255, 0.8);
+  list-style-type: none;
+`;
+
+const DeleteButton = Styled.button`
+  background-color: red;
+  color: #fff;
+  border: none;
+  outline: 0;
+  cursor: pointer;
+`;
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { recipes: [] };
+    this.state = {
+      newRecipe: {
+        internal: true,
+        name: "",
+        price: ""
+      },
+      recipes: []
+    };
 
-    // this.handleChange = this.handleChange.bind(this);
+    // this.handleChange = this.handleChange;
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.handleGetAllRecipes();
+  }
+
+  render() {
+    var recipes = this.state.recipes;
+    console.log("Recipes", recipes);
+    recipes = recipes.map((recipe, index) => {
+      return (
+        <ListLi key={index} onClick={this.handleEdit.bind(this, recipe._id)}>
+          <ListItem>{recipe.name}</ListItem>
+          <ListItem>{recipe.price}</ListItem>
+          <ListItem>{recipe.internal ? "True" : "False"}</ListItem>
+          <ListItem>
+            <DeleteButton onClick={this.handleDelete.bind(this, recipe._id)}>
+              x
+            </DeleteButton>
+          </ListItem>
+        </ListLi>
+      );
+    });
+    return (
+      <div className="">
+        <FormWrapper>
+          <h2>Add Recipe</h2>
+          <form id="search" onSubmit={this.handleSubmit}>
+            <label>
+              Enter Recipe:{" "}
+              <input
+                type="text"
+                name="name"
+                placeholder="Recipe Name?"
+                value={this.state.newRecipe.name}
+                onChange={this.onChange.bind(this)}
+              />
+            </label>
+            <br />
+            <br />
+            <label>
+              Enter Price:{" "}
+              <input
+                type="text"
+                name="price"
+                placeholder="Recipe Price?"
+                value={this.state.newRecipe.price}
+                onChange={this.onChange.bind(this)}
+              />
+            </label>
+            <br />
+            <br />
+            <label>
+              internal Recipe:{" "}
+              <input
+                type="checkbox"
+                name="internal"
+                // {this.state.newRecipe.internal == true ? "checked" : ""}
+                defaultChecked={this.state.newRecipe.internal}
+                // value={this.state.newRecipe.internal}
+                onChange={this.onChange.bind(this)}
+              />
+            </label>
+            <br />
+            <br />
+            <br />
+            <input type="submit" value="Add Recipe" />
+          </form>
+        </FormWrapper>
+        <RecipeUl>{recipes}</RecipeUl>
+      </div>
+    );
+  }
+
+  checkStatus(response) {
+    console.log(response);
+    if (response.status >= 200 && response.status < 300) {
+      return response;
+    } else {
+      var error = new Error(response.statusText);
+      error.response = response;
+      throw error;
+    }
   }
 
   handleGetAllRecipes() {
@@ -23,15 +145,30 @@ class App extends Component {
       });
   }
 
+  onChange(e) {
+    const inputName = e.target.name;
+    var inputValue = e.target.value;
+    const prevState = this.state;
+    if (inputName == "internal") {
+      var checked = false;
+      prevState.newRecipe.internal === false ? (checked = true) : "";
+      inputValue = checked;
+    }
+    this.setState(prevState => ({
+      newRecipe: {
+        ...prevState.newRecipe,
+        [inputName]: inputValue
+      }
+    }));
+  }
+
   handleSubmit(e) {
     e.preventDefault();
-    const recipeName = e.target.name.value;
-    console.log(recipeName);
+    const newRecipe = this.state.newRecipe;
+    console.log("newRecipe", newRecipe);
     return fetch("/api/recipes", {
       method: "post",
-      body: JSON.stringify({
-        name: recipeName
-      }),
+      body: JSON.stringify(newRecipe),
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json"
@@ -42,40 +179,27 @@ class App extends Component {
       .then(this.handleGetAllRecipes());
   }
 
-  checkStatus(response) {
-    console.log(response);
-    if (response.status >= 200 && response.status < 300) {
-      return response;
-    } else {
-      var error = new Error(response.statusText);
-      error.response = response;
-      throw error;
-    }
+  handleDelete(id) {
+    console.log("You Clicked", id);
+    fetch("/api/recipes/" + id, {
+      method: "delete",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+      // .then(this.checkStatus)
+      .then(() => console.log("updated!!!"))
+      .then(this.handleGetAllRecipes());
   }
 
-  render() {
-    var recipes = this.state.recipes;
-    console.log(recipes);
-    recipes = recipes.map((recipe, index) => {
-      return <li key={index}>{recipe.name}</li>;
-    });
-    return (
-      <div className="App">
-        <form id="search" onSubmit={this.handleSubmit}>
-          <label>
-            Enter Recipe:{" "}
-            <input
-              type="text"
-              name="name"
-              placeholder="Recipe Name?"
-              required
-            />
-          </label>
-          <input type="submit" value="Find Recipe" />
-        </form>
-        <ul>{recipes}</ul>
-      </div>
+  handleEdit(recipeId) {
+    const editRecipe = this.state.recipes.filter(
+      recipe => recipe._id === recipeId
     );
+    console.log("You Clicked", editRecipe);
+
+    this.setState({ editRecipe: editRecipe });
   }
 }
 
